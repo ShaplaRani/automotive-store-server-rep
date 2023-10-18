112714f11820d7651a2212367d6414e00eceb545
ShaplaRani <shaplarani621@gmail.com>
2023-10-17 19:35:15 -0700

    added the post methot in this website

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,7 +9,45 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//create a product
+//automotive
+// akZbAGGrtB2sHGSp
+
+
+const uri = "mongodb+srv://automotive:akZbAGGrtB2sHGSp@cluster0.ei0qpxt.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const automotiveCollection = client.db("automotiveDB").collection("automotive")
+     //create a product
+     app.post('/automotive' , async(req, res) => {
+        const newProduct = req.body;
+        console.log(newProduct);
+        const result = await automotiveCollection.insertOne(newProduct);
+        res.send(result);
+      })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get("/", (req, res) => {
     res.send("automotive server is running")

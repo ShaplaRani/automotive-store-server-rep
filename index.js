8e98640356f1b69bf576139c0c212ec6f0ae3a8e
ShaplaRani <shaplarani621@gmail.com>
2023-10-20 04:08:46 -0700

    updata

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//automotive
-// akZbAGGrtB2sHGSp
+
 
 // console.log(process.env.DB_USER)
 // console.log(process.env.DB_PASS)
